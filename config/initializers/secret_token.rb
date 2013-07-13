02c5cf8ea8200e4fd77f2c3fb5b815c17e85b252
Nii <niiashikwei@hotmail.com>
2013-07-13 00:39:06 -0400

    nii - initial commit, rails 3.2.13, ruby 1.9.3, root in routes directs to splash page which is currently under construction

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iamnii::Application.config.secret_token = 'c908d606f0088ced3ef303f452401d943d499923a6240614503128cded977a849be03c545a143f3cb739d24b827cb66c1513ebcbac9859ee8c335f4b38ca30d4'
