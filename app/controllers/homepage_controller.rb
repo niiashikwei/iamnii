2324a24c778ca4e4c9e4a226f41401aa43eaf503
Nii <niiashikwei@hotmail.com>
2013-08-02 11:27:59 -0400

    nii - took out slide panel toggle for now and did some optimizazion ufor google search using meta tags

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,2 +1,7 @@
 class HomepageController < ApplicationController
+  def index
+    @page_title = "IamNii - Home Page"
+    @page_description_extra = "Congratulations on reaching the Home Page for IamNii!" +
+                              " This is the best place to start exploring my content. Enjoy. "
+  end
 end
--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,2 +1,7 @@
 class SplashController < ApplicationController
+  def index
+    @page_title = "IAmNii - Splash Page"
+    @page_description_extra = "Congratulations on reaching IAmNii's famous Splash Page!" +
+                              " This page changes periodically, showcases new features as I learn them. "
+  end
 end
