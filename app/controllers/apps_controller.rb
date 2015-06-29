1af4469040339fc90ed56c3deb0417758ddde10f
Nii <niiashikwei@hotmail.com>
2015-06-29 07:21:20 +0000

    Nii - removed apps controller and scaffolding as apps will now be hosted at disruptiveideaz.com

--- app/controllers/apps_controller.rb
+++
@@ -1,7 +0,0 @@
-class AppsController < ApplicationController
-  def index
-    @page_title = "IAmNii - Apps Page"
-    @page_description_extra = "Congratulations on reaching IAmNii's famous Apps Page!" +
-        " This page changes periodically, showcases new apps that I come up with. "
-  end
-end
